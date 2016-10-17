13fde84623505cab38cb3f36606a41eba75aa73a
Steven Burgess <bsteven48@gmail.com>
2016-10-17 15:12:13 -0400

    changed beginning if else to switch statement

--- rps.js
+++ rps.js
@@ -1,21 +1,24 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.floor((Math.random*3)+1);
-if (computerChoice == 1) {
-	computerChoice = "rock";
-} else if(computerChoice == 2) {
-	computerChoice = "paper";
-} else {
-	computerChoice = "scissors";
-} console.log("Computer: " + computerChoice);
+
+switch(computerChoice){
+	case 1:
+		computerChoice = "rock";
+		break;
+	case 2:
+		computerChoice = "paper";
+		break;
+	default:
+		computerChoice = "scissors";
+		break;
+}
 
 compare(userChoice,computerChoice);
 
 var compare = function (choice1 , choice2)
 {
     if(choice1===choice2)
-        return "The result is a tie!"
-        
-        
+        return "The result is a tie!";
     else if(choice1 === "rock")
     {
         if(choice2 === "scissors")
@@ -29,18 +32,16 @@ var compare = function (choice1 , choice2)
     else if(choice1 === "paper")
     {
         if(choice2 ===" scissors")
-            return "scissors wins"
-            
+            return "scissors wins";   
         else 
-            return "paper wins"
+            return "paper wins";
     }
     
     else
     {
         if(choice2 === "rock")
-            return "rock wins"
-            
+            return "rock wins";   
         else
-            return "scissors wins"
+            return "scissors wins";
     }
 }
